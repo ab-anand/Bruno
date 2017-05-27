efb694cb5db114931b3cfe2825efc3ecebb4282f
Abhinav Anand <abhinavanand1905@gmail.com>
2017-05-27 10:11:46 +0530

    ssl error sorted

--- daily.py
+++ daily.py
@@ -1,6 +1,6 @@
 import wx
 import os
-os.environ["HTTPS_PROXY"] = "http://username:pass@192.168.1.107:3128"
+os.environ["HTTPS_PROXY"] = "http://ipg_2015003:abhi%4098@192.168.1.107:3128"
 import wikipedia
 import wolframalpha
 import time
@@ -13,10 +13,24 @@ from bs4 import BeautifulSoup
 import win32com.client as wincl
 from urllib.request import urlopen
 import speech_recognition as sr
+#Remove SSL error
+requests.packages.urllib3.disable_warnings()
+
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+
 
 headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
 speak = wincl.Dispatch("SAPI.SpVoice")
-
+time.sleep(2)
 #GUI creation
 class MyFrame(wx.Frame):
     def __init__(self):
@@ -27,12 +41,12 @@ class MyFrame(wx.Frame):
             title="BRUNO")
         panel = wx.Panel(self)
 
-        ico = wx.Icon('boy2.ico', wx.BITMAP_TYPE_ICO)
+        ico = wx.Icon('boy.ico', wx.BITMAP_TYPE_ICO)
         self.SetIcon(ico)
 
         my_sizer = wx.BoxSizer(wx.VERTICAL)
         lbl = wx.StaticText(panel,
-        label="Welcome Sir, I'm the Python Digital Assistant. How can I help you?")
+        label="Bienvenido Sir. How can I help you?")
         my_sizer.Add(lbl, 0, wx.ALL, 5)
         self.txt = wx.TextCtrl(panel, style=wx.TE_PROCESS_ENTER,size=(400,30))
         self.txt.SetFocus()
@@ -40,7 +54,7 @@ class MyFrame(wx.Frame):
         my_sizer.Add(self.txt, 0, wx.ALL, 5)
         panel.SetSizer(my_sizer)
         self.Show()
-        speak.Speak("Welcome Sir, how can I help")
+        speak.Speak("Welcome back Sir, Broono at your service. Your system is ready and operational. How can I help")
 
 
 
@@ -76,6 +90,7 @@ class MyFrame(wx.Frame):
         if put.startswith('play '):
             try:
                 link='+'.join(link[1:])
+                say=link.replace('+',' ')
                 url='https://www.youtube.com/results?search_query='+link
                 source_code = requests.get(url, headers=headers, timeout=15)
                 plain_text=source_code.text
@@ -83,7 +98,7 @@ class MyFrame(wx.Frame):
                 songs=soup.findAll('div',{'class':'yt-lockup-video'})
                 song=songs[0].contents[0].contents[0].contents[0]
                 hit=song['href']
-                speak.Speak("playing "+link)
+                speak.Speak("playing "+say)
                 webbrowser.open('https://www.youtube.com'+hit)
 
 
@@ -93,8 +108,9 @@ class MyFrame(wx.Frame):
         if put.startswith('search '):
             try:
                 link='+'.join(link[1:])
+                say=link.replace('+',' ')
                 #print(link)
-                speak.Speak("searching on google for "+link)
+                speak.Speak("searching on google for "+say)
                 webbrowser.open('https://www.google.co.in/search?q='+link)
             except:
                 print('Sorry, No internet connection!')
@@ -113,7 +129,8 @@ class MyFrame(wx.Frame):
                 jsonObj=urlopen('https://newsapi.org/v1/articles?source=new-scientist&sortBy=top&apiKey=1bb9352ea6964f539e39b431a3bcbda6')
                 data=json.load(jsonObj)
                 i=1
-                print('             ================NEW SCIENTIST===============')
+                speak.Speak('here are some top science news from new scientist')
+                print('             ================NEW SCIENTIST==============='+'\n')
                 for item in data['articles']:
                     print(str(i)+'. '+item['title']+'\n')
                     print(item['description']+'\n')
@@ -126,7 +143,8 @@ class MyFrame(wx.Frame):
                 jsonObj=urlopen('https://newsapi.org/v1/articles?source=the-times-of-india&sortBy=top&apiKey=1bb9352ea6964f539e39b431a3bcbda6')
                 data=json.load(jsonObj)
                 i=1
-                print('             ===============TIMES OF INDIA===============')
+                speak.Speak('here are some top news from the times of india')
+                print('             ===============TIMES OF INDIA==============='+'\n')
                 for item in data['articles']:
                     print(str(i)+'. '+item['title']+'\n')
                     print(item['description']+'\n')
