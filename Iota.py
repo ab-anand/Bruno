a67a11c5c8b9d5069e9e7eb57045ef17754215d0
Abhinav Anand <abhinavanand1905@gmail.com>
2017-06-01 18:11:29 +0530

    KIARA | pep8 style

--- Iota.py
+++ Iota.py
@@ -3,7 +3,8 @@ import pyttsx
 import wikipedia
 import wolframalpha
 import os
-#os.environ["HTTPS_PROXY"] = "http://username:pass@192.168.1.107:3128"
+# os.environ["HTTPS_PROXY"] = "http://user:pass@192.168.1.107:3128"
+
 
 def speak(value):
     engine = pyttsx.init()
@@ -14,48 +15,47 @@ def speak(value):
     engine.say(value)
     engine.runAndWait()
 
+
 class MyFrame(wx.Frame):
     def __init__(self):
-        wx.Frame.__init__(self,None,pos=wx.DefaultPosition,size=wx.Size(450,100),
+        wx.Frame.__init__(self, None, pos=wx.DefaultPosition,
+                          size=wx.Size(450, 100),
                           style=wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION |
                           wx.CLOSE_BOX | wx.CLIP_CHILDREN, title='KIARA')
         panel = wx.Panel(self)
         my_sizer = wx.BoxSizer(wx.VERTICAL)
-        lbl = wx.StaticText(panel,label='Hello I\'m Kiara the Python Digital Assistant. How can I help you? ')
+        lbl = wx.StaticText(panel, label='''Hello I\'m Kiara the Python
+                            Digital Assistant. How can I help you?''')
         my_sizer.Add(lbl, 0, wx.ALL, 5)
-        self.txt = wx.TextCtrl(panel, style=wx.TE_PROCESS_ENTER,size=(400,30))
+        self.txt = wx.TextCtrl(panel, style=wx.TE_PROCESS_ENTER,
+                               size=(400, 30))
         self.txt.SetFocus()
         self.txt.Bind(wx.EVT_TEXT_ENTER, self.OnEnter)
         my_sizer.Add(self.txt, 0, wx.ALL, 5)
         panel.SetSizer(my_sizer)
         self.Show()
-
         speak('Welcome my friend. I am Kiara. How can I help ?')
-        
-    
+
     def OnEnter(self, event):
         input = self.txt.GetValue()
-        #input= input.lower()
-        app_id='2V3684-LXELTTTJ9J'
+        # input = input.lower()
+        app_id = '2V3684-LXELTTTJ9J'
         try:
-            #wolframalpha
-            client=wolframalpha.Client(app_id)
-            res=client.query(input)
-            ans=next(res.results).text
-            print ans
+            # wolframalpha
+            client = wolframalpha.Client(app_id)
+            res = client.query(input)
+            ans = next(res.results).text
+            print(ans)
             speak(ans)
-            
-            
         except:
-            #wikipedia
-            input=input.split(' ')
-            input=' '.join(input[2:])
-            print wikipedia.summary(input)
+            # wikipedia
+            input = input.split(' ')
+            input = ' '.join(input[2:])
+            print(wikipedia.summary(input))
             speak('Searched wikipedia for '+input)
-           
-            
 
-if __name__=="__main__":
-    app=wx.App(True)
-    frame=MyFrame()
+
+if __name__ == "__main__":
+    app = wx.App(True)
+    frame = MyFrame()
     app.MainLoop()
